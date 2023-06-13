e23f6a01a99fd31bc4d116a69dc13d3837d8a9b0
bisesh <adhikaribisesh1590@gmail.com>
2023-06-13 02:10:22 -0400

    settings changed

--- qr/settings.py
+++ qr/settings.py
@@ -21,13 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
+SECRET_KEY = 'django-insecure-u7zl^sisu+a=2_6w35710h0ohf514ywnn8@^km+zmxsq#v=jza'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-SITE_URL = config('SITE_URL')
+SITE_URL = 'https://biseshadhikari.pythonanywhere.com/'
 
 # Application definition
 
